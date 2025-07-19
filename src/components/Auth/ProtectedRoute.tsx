91489fbba1a1d6e1e4cf6dfdd282de344febe662
Shamandeep Kaur <shamandeepkaur924@gmail.com>
2025-07-19 17:19:11 +0530

    loader style

--- src/components/Auth/ProtectedRoute.tsx
+++ src/components/Auth/ProtectedRoute.tsx
@@ -18,9 +18,13 @@ const ProtectedRoute = ({ children }) => {
 
   if (loading || !isAuthenticated) {
     return (
-      <div className="min-h-screen flex items-center justify-center">
-        <p>Loading...</p>
+       <div className="flex items-center justify-center min-h-screen">
+        <div className="flex flex-col items-center">
+          <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-indigo-600"></div>
+          <p className="mt-4 text-lg font-medium text-gray-700">Loading..</p>
+        </div>
       </div>
+
     );
   }
 
